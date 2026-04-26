920730e0003deaea1d757756cbd1dcb7c450803b
by <by965738071@163.com>
2026-04-26 22:53:02 +0800

    action build9

--- src/platform/wayland_shm_wrapper.c
+++ src/platform/wayland_shm_wrapper.c
@@ -1,8 +1,11 @@
 #include <wayland-client.h>
 #include <wayland-client-protocol.h>
 
-// 提供一个显式的 wl_shm_release 非内联实现
 void wl_shm_release(struct wl_shm *shm) {
     struct wl_proxy *proxy = (struct wl_proxy *) shm;
-    wl_proxy_marshal_flags(proxy, WL_SHM_RELEASE, NULL, wl_proxy_get_version(proxy), 0);
+    wl_proxy_marshal_flags(proxy,
+        0,                              // WL_SHM_RELEASE 的 opcode 是 0
+        NULL,
+        wl_proxy_get_version(proxy),
+        0);
 }
